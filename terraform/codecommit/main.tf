73f39253f78cebe0a7a290462a38fecf017354ce
gino8070 <>
2019-04-12 15:53:58 +0900

    added example, terraform client

---
+++ terraform/codecommit/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+module "code_commit" {
+  source      = "github.com/gino8070/aws-training//terraform_modules/code_commit"
+  name        = ""
+  description = ""
+}
